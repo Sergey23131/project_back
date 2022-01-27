7767151b4acbb838444962a77b136b06cd0aff2c
Sergey23131 <86601508+Sergey23131@users.noreply.github.com>
2022-01-27 12:44:50 +0200

    sds

--- middlewares/loginMiddleware.js
+++ middlewares/loginMiddleware.js
@@ -37,7 +37,7 @@ module.exports = {
             if (error) {
                 throw new ErrorHandler(errors_massage.NOT_VALID_BODY, errors_code.NOT_VALID);
             }
-            
+
             req.body = value;
 
             next();
